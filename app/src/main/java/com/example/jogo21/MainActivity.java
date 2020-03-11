95d2852908c71e1b3df19be278686e6c47c1b656
Rojie <33533062+RojieEmanuel@users.noreply.github.com>
2020-03-11 19:27:49 -0300

    Icone Mudado
    Mostrar Senha no Login implementado

--- app/src/main/java/com/example/jogo21/MainActivity.java
+++ app/src/main/java/com/example/jogo21/MainActivity.java
@@ -4,16 +4,18 @@ package com.example.jogo21;
 
         import android.content.Intent;
         import android.os.Bundle;
+        import android.text.method.PasswordTransformationMethod;
         import android.util.Log;
         import android.view.View;
         import android.widget.Button;
         import android.widget.CheckBox;
+        import android.widget.CompoundButton;
         import android.widget.EditText;
         import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    TextView TituloLogin;
+
     EditText editTextLogin;
     EditText editTextSenha;
     CheckBox checkBoxVisualizar;
@@ -29,7 +31,23 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        editTextLogin = (EditText)findViewById(R.id.editTextLogin);
+        editTextSenha = (EditText)findViewById(R.id.editTextSenha);
+        checkBoxVisualizar = (CheckBox)findViewById(R.id.checkBoxVisualizar);
+        buttonLogin = (Button)findViewById(R.id.buttonLogin);
         buttonCadastro = (Button)findViewById(R.id.buttonCadastro);
+
+        checkBoxVisualizar.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (!isChecked) {
+                    editTextSenha.setTransformationMethod(new PasswordTransformationMethod());
+                } else {
+                    editTextSenha.setTransformationMethod(null);
+                }
+            }
+        });
     }
 
     public void onClickbuttonLogin(View v){
